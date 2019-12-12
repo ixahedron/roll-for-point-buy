e71977f502c77bc40b2902f620cb11c694972ed5
Ilona <ixahedron@users.noreply.github.com>
2019-12-12 01:27:47 +0100

    Rewrite to allow boundaries

--- bot.js
+++ bot.js
@@ -2,36 +2,113 @@ var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 var roll = require('./roll.js')
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+    token: auth.token,
+    autorun: true
 });
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Listen for messages starting with /pb or /help
-    if (message.substring(0, 3) == '/pb') {
-      //m = "Rolling 4d6 drop lowest: ";
-      bot.sendMessage({
-          to: channelID,
-          message: roll.roll(6)
-      });
-    }
-    else if (message.substring(0, 5) == '/help') {
-      bot.sendMessage({
-          to: channelID,
-          message: "Use /pb to roll 4d6 drop lowest 6 times."
-      });
+
+
+// continue to reroll until the result is within provided boundaries
+function roll_bounded(n, min, max) {
     
+    do {
+      roll_result = roll.roll(n);
+    } while ((roll_result.pb_sum < min) || (roll_result.pb_sum > max));
+
+    return roll_result;
+
+}
+
+function pretty_print_msg(roll_obj) {
+    return roll_obj.stats + '. Point buy value: ' + roll_obj.pb_sum;
+}
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+
+    // Listen for messages starting with /pb or /help
+    if (message.substring(0, 1) == '/') {
+
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+        args = args.splice(1);
+        
+        switch(cmd) {
+            case 'pb':
+
+                var n = 6;
+                
+                if (!args) {
+
+                    bot.sendMessage({
+                        to: channelID,
+                        message: pretty_print_msg(roll.roll(n))
+                    });
+                    break;
+
+                }
+
+                // check args are not NaN
+                if (!isNaN(args[0])) {
+                    n = args[0];
+                    if (n > 30) {
+                        n = 30;
+                    }
+                }
+                args.splice(0,1);
+                
+                // establish default boundaries incase not all are provided. Dummy values are dummy.
+                var min = n*(-99);
+                var max = n*99;
+
+                if (!isNaN(args[0])) {
+                    min = args[0];
+                }
+                args.splice(0,1);
+                if (!isNaN(args[0])) {
+                    max = args[0];
+                }
+
+                // swap to not go into an endless cycle
+                if (min > max){
+                    var tmp;
+                    tmp = min;
+                    min = max;
+                    max = tmp;
+                }
+
+                // send a pretty printed message.
+                bot.sendMessage({
+                    to: channelID,
+                    message: pretty_print_msg(roll_bounded(n, min, max))
+                });
+                break;
+            case 'pbi': // just for my own games since that's what I use
+                bot.sendMessage({
+                    to: channelID,
+                    message: pretty_print_msg(roll_bounded(6, 24, 40))
+                });
+                break;
+            case 'help':
+                bot.sendMessage({
+                    to: channelID,
+                    message: "Use /pb to roll 4d6 drop lowest 6 times.\nSpecify three or less numbers to bound the roll, like this: /pb number_of_abilities min_accepted_cost max_accepted_cost.\nE.g. '/pb 6 24 40' is what I use for my game."
+                });
+        }
+
     }
+
 });
--- roll.js
+++ roll.js
@@ -1,10 +1,11 @@
 function roll_dn(sides) {
 
-  var dn = Math.floor(Math.random() * sides + 1); // generate random number between 0 and 1
-  return(dn); // show popup with a random number
+  var dn = Math.floor(Math.random() * sides + 1); // generate random number between 1 and sides
+  return(dn);
   
 }
 
+// roll 4d6, dropping the lowest result.
 function roll_4d6d1() {
   
   var arr = [roll_dn(6), roll_dn(6), roll_dn(6), roll_dn(6)]
@@ -14,6 +15,7 @@ function roll_4d6d1() {
 
 }
 
+// lookup point buy costs
 function point_buy(stat) {
 
   var t = [NaN, NaN, NaN, -9, -6, -4, -2, -1, 0, 1, 2, 3, 4, 5, 7, 9, 12, 15, 19];
@@ -23,25 +25,13 @@ function point_buy(stat) {
 
 module.exports = {
   roll: function(nabilities) {
-    var pb_sum = 0;
-    var m = "";
 
-    for (var i=1; i<=nabilities; i=i+1) {
+    // roll 4d6d1 n times
+    var stats = Array.apply(null, {length: nabilities}).map(Function.call, roll_4d6d1);
 
-      // roll 4d6d1 n times and sum up their point buy costs
-      var stat = roll_4d6d1();
-      pb_sum = pb_sum + point_buy(stat);
-      m = m + stat;
+    var pb_sum = stats.reduce((acc, stat) => acc + point_buy(stat), 0);
 
-      if (i<nabilities) {
-        m = m + ", ";
-      }
-    }
-
-    m = m + ". Point buy value: " + pb_sum;
-
-    return m;
-
+    return {stats: stats, pb_sum: pb_sum};
   }
-}
 
+}
